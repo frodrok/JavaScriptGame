f0fc1f61727fb3a04441459972b8a6853ca8f6cd
frodrok <fredrik.hollinger@gmail.com>
2016-09-07 13:08:43 +0200

    added js file and gitignore file

--- js/base.js
+++ js/base.js
@@ -1,1 +1,24 @@
 console.log("start");
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    console.log(e);
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+        // left arrow
+    }
+    else if (e.keyCode == '39') {
+        // right arrow
+    }
+
+}
