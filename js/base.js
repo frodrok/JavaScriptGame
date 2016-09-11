eedac7ed5c02679e9b5afeeef58d811494d4c66d
Izumi Suzuki <lindabjalla@gmail.com>
2016-09-12 00:00:28 +0200

    add create trails function

--- js/base.js
+++ js/base.js
@@ -10,8 +10,8 @@ var monster = {symbol: 'X', x: 1, y: 2};
 var playing = false;
 
 function startGame() {
-
     playing = true;
+    generateMap();
 
     while (playing) {
 
@@ -21,6 +21,7 @@ function startGame() {
 
         var input = prompt('Which direction would you like to go? [u]p, [d]own, [l]eft or [r]ight. Enter [s]top to stop getting prompts.');
 
+        replacePlayerToTrail();
         handleInput(input);
     }
 }
@@ -67,9 +68,14 @@ function moveDown() {
     player.x = player.x + 1;
 }
 
+/* the player tile becomes a floor tile after the player left */
+function replacePlayerToTrail(){
+    map[player.x][player.y] = mapLayout.floor;
+}
+
 /* make a new - clear - map and puts the player and monsters */
 function updateMap() {
-    map = generateMap();
+    // map = generateMap();
     map[monster.x][monster.y] = monster.symbol;
     map[player.x][player.y] = player.symbol;
 }
@@ -83,8 +89,8 @@ function printArray(multiArray) {
 
 /* creates a new clear map */
 function generateMap() {
-    var wholeArray = new Array(15);
 
+    var wholeArray = new Array(15);
 
     for (var i = 0; i < 15; i++) {
         var oneRow = new Array(15);
@@ -96,11 +102,14 @@ function generateMap() {
                 oneRow[j] = mapLayout.walls;
             } else {
 
+                var randomTile = createTrails();
+
                 /* left most tile and right most tile should be wall */
                 if (j == 0 || j == 14) {
                     oneRow[j] = mapLayout.walls;
                 } else {
-                    oneRow[j] = mapLayout.floor;
+                    // oneRow[j] = mapLayout.floor;
+                    oneRow[j] = randomTile;
                 }
             }
         }
@@ -108,5 +117,12 @@ function generateMap() {
         wholeArray[i] = oneRow;
     }
 
-    return wholeArray;
+    map = wholeArray;
+    return map;
+}
+
+/* get a random value to generate a floor or a wall tile randomly */
+function createTrails(){
+
+    return Math.random() < 0.5 ? mapLayout.floor : mapLayout.walls;
 }
